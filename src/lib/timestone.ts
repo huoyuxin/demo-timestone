bb15a94a010b0d5ecfbbe37016869afbdd8a7069
Yuxin <yuxin.huo@shopee.com>
2023-04-19 19:38:24 +0800

    feat: env split

--- src/lib/timestone.ts
+++ src/lib/timestone.ts
@@ -1,16 +1,22 @@
 import {timestoneTracker} from '@timestone/tracker';
 import {Operation, Pages} from '@timestone/tracker/types/interface';
 
+const prodConfig = {
+  api: 'https://timestone-api.datagroup.shopeemobile.com/open/rules',
+  // Site Management 的 appId
+  appId: ['50e6cb6acfab04af074e9c9d14ef19e3'],
+  cdnUrl: 'https://bigdata-timestone-1255300759.cossgp.myqcloud.com',
+};
+const devConfig = {
+  api: 'https://timestone-api.datagroup.uat.shopeemobile.com/open/rules',
+  // Site Management 的 appId
+  appId: ['74f73dfe39ee9e7b85e11f9942f8d7d3'],
+  cdnUrl: 'https://bigdata-timestone-1255300759.cossgp.myqcloud.com',
+};
+
 const config = {
-  // todo: 区分 uat \ live 环境
-  // todo: 区分 uat \ live 环境
-  // todo: 区分 uat \ live 环境
-  requestConfig: {
-    api: 'https://timestone-api.datagroup.uat.shopeemobile.com/open/rules',
-    // Site Management 的 appId
-    appId: ['74f73dfe39ee9e7b85e11f9942f8d7d3'],
-    cdnUrl: 'https://bigdata-timestone-1255300759.cossgp.myqcloud.com',
-  },
+  // 区分 prod \ dev 环境
+  requestConfig: process.env.REACT_APP_ENV === 'prod' ? prodConfig : devConfig,
 
   // 数据携带，参考文档：https://confluence.shopee.io/pages/viewpage.action?pageId=1680291321&src=contextnavpagetreemode
   // 1. 全局数据，需要在 Global Config/ Global Data 注册字段。埋点触发时执行，实时获取
@@ -72,9 +78,9 @@ const config = {
 // 根据 TS 类型提示
 const trackerConfig = {
   version: 'v4',
-  appId: 23,
+  appId: process.env.REACT_APP_ENV === 'prod' ? 105 : 305,
   platform: 'pc_web' as 'pc_web',
-  env: 'test',
+  env: process.env.REACT_APP_ENV === 'prod' ? 'live' : 'test',
   locale: 'sg',
   getUserId: () => 6,
 };
